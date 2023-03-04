5e3471294eb23871a5589776d7c4577329ac5ce2
leestephen0320 <leestephen0320@gmail.com>
2023-03-04 21:51:34 +0000

    modified:   src/main/java/ContainsDuplicates.java

--- src/main/java/ContainsDuplicates.java
+++ src/main/java/ContainsDuplicates.java
@@ -1,4 +1,4 @@
-
+import java.util.*;
 
 public class ContainsDuplicates {
     /**
@@ -8,6 +8,14 @@ public class ContainsDuplicates {
      * @return true if nums contains any duplicate values, false if it does not.
      */
     public boolean containsDuplicate(int[] nums){
+        Set<Integer> numsSet = new HashSet<Integer>();
+        for(int i = 0; i < nums.length; i++){
+            if(numsSet.contains(nums[i])){
+                return true;
+            }
+            numsSet.add(nums[i]);
+        }  
         return false;
     }
-}
+    }
+
